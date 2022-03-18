8a864727cfb05b1511872acb73c290fa8316651d
0xTranqui <thinkandchill11@gmail.com>
2022-03-17 23:00:38 -0300

    fix 2 took out hyperlink

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       <nav className="border-b p-6">
-        <p className="text-4xl font-bold"><a className="underline text-blue-600 hover:text-blue-800 visited:text-pink-600" href="https://twitter.com/0xTranqui">tranqui.eth's</a> NFT Marketplace</p>
+        <p className="text-4xl font-bold"> NFT Marketplace</p>
         <div className="flex mt-4">
           <Link href="/">
             <a className="mr-4 text-pink-500">
